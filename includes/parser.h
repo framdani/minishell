e08c3846409c0a91b17428c41aaf2ad42c803a58
framdani <fatimazahra.ramdani@usmba.ac.ma>
2021-07-05 17:59:16 +0100

    parser is normed

--- includes/parser.h
+++ includes/parser.h
@@ -3,58 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akhalidy <akhalidy@student.42.fr>          +#+  +:+       +#+        */
+/*   By: framdani <framdani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 17:27:45 by framdani          #+#    #+#             */
-/*   Updated: 2021/07/02 15:47:44 by akhalidy         ###   ########.fr       */
+/*   Updated: 2021/07/05 17:53:52 by framdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
-
-#include "lexer.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include "lexer.h"
-#include "../libft/libft.h"
+# include "lexer.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include "lexer.h"
+# include "../libft/libft.h"
 
 # define INPUT = 0
 # define OUTPUT = 1
-/* List of args = SUFFIX
- *
- * option
- * simple arg
- * redirection filename
- *
- * */
-typedef struct			s_arg
+typedef struct s_arg
 {
 	char				*value;
 	struct s_arg		*next;
 }						t_arg;
 
-typedef struct			s_file
+typedef struct s_file
 {
 	char				*filename;
 	int					type;
 	struct s_file		*next;
 }						t_file;
-/*
- * cmd name is the first arg
- * linked_list of args
- * nbr_args = size of t_args
- *
- */
+
 typedef struct s_envv
 {
 	char	*name;
 	char	*value;
 }			t_envv;
 
-typedef struct			s_cmd
+typedef struct s_cmd
 {
 	char				**args;
 	t_file				*file;
@@ -62,45 +49,23 @@ typedef struct			s_cmd
 	struct s_cmd		*next;
 }						t_cmd;
 
-/*
- * Pipe sequence : simple commands
- *	              |pipe sequence
- *	Linked list of commands
- *  number of commands = size of t_cmd
- *
- */
-
-typedef struct			s_pipe_sequence
-{
-	t_cmd				*cmds;
-	int					nbr_pipe; // cmds
-}						t_pipe_sequence;
-
-/*
- *
- * Env_var
- *
- */
-
-int	ft_lst_size(t_arg	*lst_arg);
-char 	*parse_line_hd(char *input, t_list **envl);
-t_token *parser(t_token *lexer);
+int		ft_lst_size(t_arg	*lst_arg);
+char	*parse_line_hd(char *input, t_list **envl);
+t_token	*parser(t_token *lexer);
 char	*expander(t_token **lst, char *input, t_list **envl, int state);
-char 	*expander_inside_dquote(t_token **lst_token, char *input, t_list **envl, int state);
-char 	*expander_spec_case(t_token **lst_token, char *input, t_list **envl);
+char	*expander_inside_dquote(t_token **lst_token, char *input, t_list **envl,
+			int state);
+char	*expander_spec_case(t_token **lst_token, char *input, t_list **envl);
 void	print_error_and_exit(t_token **lst_token, int error);
-t_cmd	*fill_struct(t_token *token); //t_list **envl);
+t_cmd	*fill_struct(t_token *token);
 void	add_arg(t_arg **lst_arg, char *arg);
 void	add_cmd(t_cmd **lst_cmds, t_arg **args, t_file **files);
 void	add_file(t_file **lst_files, char *filename, int type);
 void	print_struct(t_cmd *lst_cmnds);
-t_cmd	init_cmd(void);
-t_file	init_file(void);
-t_arg	init_args(void);
 char	**convert_into_dpointer(t_arg **lst_arg);
 void	free_lst_files(t_file **lst_files);
 void	free_lst_args(t_arg **lst_args);
-void 	free_lst_cmds(t_cmd **lst_cmds);
+void	free_lst_cmds(t_cmd **lst_cmds);
 int		is_redirection(int type);
 t_envv	*get_key_value(char	*arg);
 void	print_error(char *str);
