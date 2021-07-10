95bbec396add0128b10c73eb8f2231514b370bfd
framdani <fatimazahra.ramdani@usmba.ac.ma>
2021-07-10 18:14:47 +0100

    fix CTRL-D

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: akhalidy <akhalidy@student.42.fr>          +#+  +:+       +#+        */
+/*   By: framdani <framdani@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 12:41:12 by framdani          #+#    #+#             */
-/*   Updated: 2021/07/10 16:43:13 by akhalidy         ###   ########.fr       */
+/*   Updated: 2021/07/10 18:14:20 by framdani         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,20 +114,15 @@ int	main(int argc, char **argv, char **env)
 		cmd_line = readline("minishell$ ");
 		if (cmd_line == NULL)
 		{
-			ft_putstr_fd("exit", 1);
-			rl_on_new_line();
-			// rl_replace_line("exit", 0);
-			// system("leaks minishell");
+			ft_putstr_fd("\033[Aminishell$ exit\n", 1);
 			exit(g_help.ret);
 		}
 		if (*cmd_line)
 			add_history(cmd_line);
 		size = ft_strlen(cmd_line) + 1;
 		tokens = lexer(cmd_line, size, &envl);
-		//print_lexer(tokens);
 		tokens = parser(tokens);
 		lst_cmds = fill_struct(tokens);
-		//print_struct(lst_cmds);
 		if (lst_cmds != NULL)
 			ft_launch_execution(lst_cmds, &envl);
 		system("leaks minishell");
